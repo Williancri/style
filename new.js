d1df0c9f295cdd5e64d58e4fde737ec1ccf69392
willian <williancristian@gmail.com>
2021-10-18 20:55:43 -0300

    commit3

---
+++ new.js
@@ -0,0 +1,4 @@
+const firstname = 'willian';
+const lastname = 'nascimento';
+const fullname = firstname + '  ' + lastname;
+console.log(fullname);
---
+++ treio.js
@@ -0,0 +1,19 @@
+//if
+/*const idadedoosuario = 35;
+if   (idadedoosuario >= 18 )       {
+    console.log('osuario maior de idade')
+}else{
+    console.log('osuario menor dde idade')
+}
+*/
+
+//if, else, 
+/*const velocidade = 200;
+if (velocidade >= 100) {
+    console.error('multa gravissima');
+} else if (velocidade >= 80 && velocidade < 100){
+    console.warn('multa grave');
+}else if (velocidade>= 60 && velocidade < 80){
+    console.log('multa leve');
+}
+*/
